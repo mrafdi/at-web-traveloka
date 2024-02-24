6734205a532ccfd4e2b353099bac1e6594f8f03f
M <muhrafdis@gmail.com>
2024-02-24 23:30:45 +0700

    update 1to50

--- cypress/e2e/1to50.cy.js
+++ cypress/e2e/1to50.cy.js
@@ -1,4 +1,5 @@
 const numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50]
+const element=`div[style="opacity: 1;"]`
 
 before(() => {
     cy.visit("https://zzzscore.com/1to50/en/");
@@ -7,8 +8,12 @@ before(() => {
 describe("Traveloka Take Home Test", () => {
     it("Does Challenge using Automation Test for problem no.4", {scrollBehavior: "center"}, () => {
         for(let i=1; i <= numbers.length; i++) {
-            cy.get(`div[style="opacity: 1;"]`).contains(i).scrollIntoView().click({force: true});
+            cy.get(element).contains(i).scrollIntoView().click({force: true});
             // cy.get(`div[style="opacity: 1;"]`).contains(i).scrollIntoView().trigger("click"); // this code has the same effect as above
         }
     });        
 });
+
+/*
+    Somehow the test failed, as I stated on the PDF file. Since the click is not having any effect, the test will fail on the 26th iteration
+*/
